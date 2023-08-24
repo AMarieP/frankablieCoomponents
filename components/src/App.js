f6f9e132b9f535733164283715f372dff315488c
A <alyssa@kvasl.com>
2023-08-24 17:39:16 +1200

    Removed heading comp, styles w CSS and import font

--- components/src/App.js
+++ components/src/App.js
@@ -4,11 +4,13 @@ import ProfileImage from './component/about/ProfileImage';
 import pfpplaceholder from './images/pfp_placeholder.jpg';
 import pfpplaceholder2 from './images/pfpplaceholder2.jpg';
 
-
 function App() {
   return (
     <>
     <ProfileImage image={pfpplaceholder2} />
+    <h1>Hello</h1>
+    <h2>Hello</h2>
+    <h3>Hello</h3>
     </>
   );
 }
