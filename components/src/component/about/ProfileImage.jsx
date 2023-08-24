d92c5e1fa33c4b4b9ce61050a20c9894ed9c1c01
A <alyssa@kvasl.com>
2023-08-24 17:21:29 +1200

    Create profile picture component

---
+++ components/src/component/about/ProfileImage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+export default function profileImage({image}) {
+
+  return (
+    <div className='main' style={styles.main}>
+      <img className='image' style={styles.image} src={image} alt="Frank Albie's Creator, Gina. She is a young adult with light brown hair, glasses and a big smile. " />
+    </div>
+  )
+}
+
+const styles = {
+  main: {
+    margin: '1rem',
+  },
+  image: {
+    borderTopLeftRadius: '50%',
+    borderTopRightRadius: '50%',
+    border: '0.1rem solid #354622',
+    objectFit: 'cover',
+  },
+}
