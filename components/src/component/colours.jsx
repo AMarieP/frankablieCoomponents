be7e257cdf3075ebee303b7f3c2a7fbb10919701
A <alyssa@kvasl.com>
2023-08-26 14:49:35 +1200

    Created colours document

---
+++ components/src/component/colours.jsx
@@ -0,0 +1,13 @@
+
+const colours = {
+    yellow: '#FEFAE1',
+    lightYellow: '#FFFDF3',
+    pink: '#F8DBCD',
+    lightPink: '#FFEFE7',
+    khaki: '#CAC38C',
+    lightKhaki: '#D4D0B0',
+    green: '#354622',
+    lightGreen: '#697759'
+}
+
+export default colours
