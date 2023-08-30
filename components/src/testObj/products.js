01d2222c5920ce2b0f1719c9fabcf033098e097f
A <alyssa@kvasl.com>
2023-08-30 23:12:05 +1200

    Sample products moved to new file

---
+++ components/src/testObj/products.js
@@ -0,0 +1,33 @@
+
+import testProdImage from '../images/productimage_test.jpeg'
+
+
+const productTest = {
+    name: 'test',
+    imageUrl: testProdImage,
+    id: 1
+  }
+  const productTest2 = {
+    name: 'test',
+    imageUrl: testProdImage,
+    id: 2
+  }
+  const productTest3 = {
+    name: 'testing',
+    imageUrl: testProdImage,
+    id: 3
+  }
+  const productTest4 = {
+    name: 'test',
+    imageUrl: testProdImage,
+    id: 4
+  }
+  const productTest5 = {
+    name: 'test',
+    imageUrl: testProdImage,
+    id: 5
+  }
+  
+  const listProdTest = [productTest, productTest2, productTest3, productTest4, productTest5]
+
+  export default listProdTest
